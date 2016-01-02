f6376071f2ff0b79a9272cc63bacafa6988d0a00
Mohammad Arsalan Rajput <a4arshi@yahoo.com>
2016-01-02 18:21:46 +0500

    todo app

---
+++ app.ts
@@ -0,0 +1,34 @@
+/// <reference path="typings/tsd.d.ts" />
+
+angular.module("myApp",['ngMaterial','ngMdIcons'])
+.controller("myCtrl",function(filterFilter){
+   var  self = this;
+    self.Todo = [{
+        todo : "Todo",
+        selected : false
+    }];
+    this.done = 1;
+    this.getTotalTodo = function(){
+        return self.Todo.length;
+    }
+    this.archive =function(index){
+        if(!self.Todo[index].selected){
+         self.done--;    
+    }else {
+        self.done++;
+    }
+    }
+    this.Archive = function(){
+             self.Todo = filterFilter(self.Todo ,function(addtodo){
+                    return ! addtodo.selected  
+             })
+         
+     }
+    
+    this.Add = function(){
+       self.Todo.push({todo : self.TodoData.todo , selected : false});
+       self.done++;
+       self.TodoData.todo= "";
+    }
+ 
+})
